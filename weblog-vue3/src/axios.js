b2072e4cf2b9d0e4715bf734e08597da0103f97b
abSwitchJ <3154503097@qq.com>
2026-04-12 21:44:11 +0800

    update 生产环境和开发环境的vuebaseURL

--- weblog-vue3/src/axios.js
+++ weblog-vue3/src/axios.js
@@ -5,7 +5,7 @@ import { useUserStore } from '@/stores/user'
 
 // 创建 Axios 实例
 const instance = axios.create({
-    baseURL: "/api", // 你的 API 基础 URL
+    baseURL: import.meta.env.VITE_APP_BASE_API, // 你的 API 基础 URL
     timeout: 7000, // 请求超时时间
 })
 
